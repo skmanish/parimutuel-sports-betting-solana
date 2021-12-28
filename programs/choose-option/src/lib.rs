26f6e9114acdbcda5c99c3daed5e80b9ceda7a75
Manish <manish.mks.999@gmail.com>
2021-12-29 01:55:40 +0530

    START and END event instructions for EventAccount

--- programs/choose-option/src/lib.rs
+++ programs/choose-option/src/lib.rs
@@ -8,12 +8,40 @@ mod choose_option {
 
     pub fn initialize_event(
         ctx: Context<InitializeEventAccount>, 
-        event_id: u32, 
+        event_id: u32,
+        authority: Pubkey, 
         vault: Pubkey
     ) -> ProgramResult {
         let event_account = &mut ctx.accounts.event_account;
         event_account.event_id = event_id;
+        event_account.authority = authority;
         event_account.vault = vault;
+        event_account.option_1_balance_cents = 0;
+        event_account.option_2_balance_cents = 0;
+        event_account.option_3_balance_cents = 0;
+        event_account.option_4_balance_cents = 0;
+        event_account.option_5_balance_cents = 0;
+        event_account.correct_option_number = std::u8::MAX;
+        event_account.state = 0;
+        Ok(())
+    }
+
+    pub fn set_event_started(ctx: Context<StartEvent>) -> ProgramResult {
+        let event_account = &mut ctx.accounts.event_account;
+        event_account.option_1_balance_cents = 0;
+        event_account.option_2_balance_cents = 0;
+        event_account.option_3_balance_cents = 0;
+        event_account.option_4_balance_cents = 0;
+        event_account.option_5_balance_cents = 0;
+        event_account.correct_option_number = std::u8::MAX;
+        event_account.state = 1;
+        Ok(())
+    }
+
+    pub fn set_event_ended(ctx: Context<EndEvent>) -> ProgramResult {
+        let event_account = &mut ctx.accounts.event_account;
+        event_account.correct_option_number = std::u8::MAX;
+        event_account.state = 2;
         Ok(())
     }
 
@@ -58,10 +86,15 @@ pub struct BaseAccount {
 #[account]
 pub struct EventAccount {
     pub event_id: u32,
+    pub authority: Pubkey,
     pub vault: Pubkey,
 
-    // Event State must be oneof {CREATED, OPEN, CLOSED, RESOLVED}.
-    pub state: String,  
+    // Event State must be oneof 
+    // 0: CREATED,
+    // 1: STARTED,
+    // 2: ENDED, 
+    // 3: RESOLVED,
+    pub state: u8,  
 
     // Balance in cents means SOL*100. So, 0.1 SOL means 10 SOL cents.
     pub option_1_balance_cents: u32,
@@ -76,9 +109,29 @@ pub struct EventAccount {
 
 #[derive(Accounts)]
 pub struct InitializeEventAccount<'info> {
-    #[account(init, payer = admin, space = 75)]
+    #[account(init, payer = authority, space = 110)]
     pub event_account: Account<'info, EventAccount>,
     #[account(mut)]
-    pub admin: Signer<'info>,
+    pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct StartEvent<'info> {
+    pub authority: Signer<'info>,
+    #[account(mut, has_one = authority)]
+    pub event_account: Account<'info, EventAccount>,
+}
+
+#[derive(Accounts)]
+pub struct EndEvent<'info> {
+    pub authority: Signer<'info>,
+    #[account(mut, has_one = authority)]
+    pub event_account: Account<'info, EventAccount>,
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("Cannot initialize an EventAccount because event_id already exists!")]
+    InitializeEventFailsAlreadyExists,
 }
--- tests/choose-option.js
+++ tests/choose-option.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const anchor = require("@project-serum/anchor");
-const { SystemProgram } = anchor.web3;
+const { SendTransactionError, SystemProgram } = anchor.web3;
 
 describe("ChooseOption", () => {
   let _baseAccount;
@@ -42,4 +42,54 @@ describe("ChooseOption", () => {
     console.log('All data: ', account.dataList);
     assert.ok(account.dataList.length === 2);
   });
+
+  // EventAccount Instructions.
+  it("Initialize an EventAccount with random event_id and vault", async () => {
+    const program = anchor.workspace.ChooseOption;
+    const dummyEventAccount = anchor.web3.Keypair.generate();
+    const dummyVaultAccount = anchor.web3.Keypair.generate();
+    // Returns a random integer from 0 to 99:
+    const randomEventId = Math.floor(Math.random() * 100);
+    await program.rpc.initializeEvent(randomEventId, provider.wallet.publicKey, dummyVaultAccount.publicKey, {
+      accounts: {
+        eventAccount: dummyEventAccount.publicKey,
+        authority: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [dummyEventAccount],
+    });
+    const eventAccount = await program.account.eventAccount.fetch(dummyEventAccount.publicKey);
+    assert.ok(eventAccount.eventId === randomEventId);
+    assert.ok(eventAccount.option1BalanceCents === 0);
+    assert.ok(eventAccount.option2BalanceCents === 0);
+    assert.ok(eventAccount.option3BalanceCents === 0);
+    assert.ok(eventAccount.option4BalanceCents === 0);
+    assert.ok(eventAccount.option5BalanceCents === 0);
+    assert.deepStrictEqual(eventAccount.authority, provider.wallet.publicKey);
+    assert.deepStrictEqual(eventAccount.vault, dummyVaultAccount.publicKey);
+  });
+  it("Initialize an EventAccount with pre-existing event_id should fail", async () => {
+    const program = anchor.workspace.ChooseOption;
+    const dummyEventAccount = anchor.web3.Keypair.generate();
+    const dummyVaultAccount = anchor.web3.Keypair.generate();
+    const randomEventId = 5;
+
+    const initializeEvent = async () => {
+      await program.rpc.initializeEvent(randomEventId, provider.wallet.publicKey, dummyVaultAccount.publicKey, {
+        accounts: {
+          eventAccount: dummyEventAccount.publicKey,
+          authority: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [dummyEventAccount],
+      });
+      return await program.account.eventAccount.fetch(dummyEventAccount.publicKey);
+    }
+    await initializeEvent();
+    await assert.rejects(initializeEvent, SendTransactionError)
+  });
 });
+
+
+// For a UserAccount, implement JoinEvent, ChooseOption
+// Alongside ChooseOption instruction, EventAccount should also be touched
