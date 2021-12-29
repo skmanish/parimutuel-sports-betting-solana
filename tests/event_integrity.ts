1678010845e5a2c2d2ef37407109b08152419a34
Manish <manish.mks.999@gmail.com>
2021-12-29 16:44:28 +0530

    Add eslint for tests formatting

--- tests/event_integrity.ts
+++ tests/event_integrity.ts
@@ -1,70 +1,82 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import BN from 'bn.js';
-import chai, { assert, expect } from 'chai';
+import {Program} from '@project-serum/anchor';
+import chai, {assert, expect} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 chai.use(chaiAsPromised);
 
-const { SystemProgram} = anchor.web3;
-import { PublicKey, Keypair, SendTransactionError} from '@solana/web3.js';
-
+const {SystemProgram} = anchor.web3;
+import {Keypair, SendTransactionError} from '@solana/web3.js';
 
 describe('EventIntegrity', () => {
-    const provider = anchor.Provider.env();
-    anchor.setProvider(provider);
-    const program = anchor.workspace.ChooseOption as Program;
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.ChooseOption as Program;
 
-    it("Initialize an EventAccount with random event_id and vault", async () => {
-        const dummyEventAccount: Keypair = anchor.web3.Keypair.generate();
-        const dummyVaultAccount: Keypair = anchor.web3.Keypair.generate();
-        // Returns a random integer from 0 to 99:
-        const randomEventId = Math.floor(Math.random() * 100);
+  it('Initialize an EventAccount with random event_id and vault', async () => {
+    const dummyEventAccount: Keypair = anchor.web3.Keypair.generate();
+    const dummyVaultAccount: Keypair = anchor.web3.Keypair.generate();
+    // Returns a random integer from 0 to 99:
+    const randomEventId = Math.floor(Math.random() * 100);
 
-        await program.rpc.initializeEvent(randomEventId, provider.wallet.publicKey, dummyVaultAccount.publicKey, {
+    await program.rpc.initializeEvent(
+        randomEventId,
+        provider.wallet.publicKey,
+        dummyVaultAccount.publicKey,
+        {
           accounts: {
             eventAccount: dummyEventAccount.publicKey,
             authority: provider.wallet.publicKey,
             systemProgram: SystemProgram.programId,
           },
           signers: [dummyEventAccount],
-        });
-        const eventAccount: any = await program.account.eventAccount.fetch(dummyEventAccount.publicKey);
-        assert.ok(eventAccount.eventId === randomEventId);
-        assert.ok(eventAccount.option1BalanceCents === 0);
-        assert.ok(eventAccount.option2BalanceCents === 0);
-        assert.ok(eventAccount.option3BalanceCents === 0);
-        assert.ok(eventAccount.option4BalanceCents === 0);
-        assert.ok(eventAccount.option5BalanceCents === 0);
-        assert.deepStrictEqual(eventAccount.authority, provider.wallet.publicKey);
-        assert.deepStrictEqual(eventAccount.vault, dummyVaultAccount.publicKey);
-      });
+        },
+    );
+    const eventAccount: any = await program.account.eventAccount.fetch(
+        dummyEventAccount.publicKey,
+    );
+    assert.ok(eventAccount.eventId === randomEventId);
+    assert.ok(eventAccount.option1BalanceCents === 0);
+    assert.ok(eventAccount.option2BalanceCents === 0);
+    assert.ok(eventAccount.option3BalanceCents === 0);
+    assert.ok(eventAccount.option4BalanceCents === 0);
+    assert.ok(eventAccount.option5BalanceCents === 0);
+    assert.deepStrictEqual(eventAccount.authority, provider.wallet.publicKey);
+    assert.deepStrictEqual(eventAccount.vault, dummyVaultAccount.publicKey);
+  });
 
-      it("Initialize an EventAccount with pre-existing event_id should fail", async () => {
+  it('Initialize an EventAccount with pre-existing event_id should fail',
+      async () => {
         const program = anchor.workspace.ChooseOption;
         const dummyEventAccount = anchor.web3.Keypair.generate();
         const dummyVaultAccount = anchor.web3.Keypair.generate();
         const randomEventId = 5;
-    
+
         const initializeEvent = async () => {
-          await program.rpc.initializeEvent(randomEventId, provider.wallet.publicKey, dummyVaultAccount.publicKey, {
-            accounts: {
-              eventAccount: dummyEventAccount.publicKey,
-              authority: provider.wallet.publicKey,
-              systemProgram: SystemProgram.programId,
-            },
-            signers: [dummyEventAccount],
-          });
-          return await program.account.eventAccount.fetch(dummyEventAccount.publicKey);
-        }
+          await program.rpc.initializeEvent(
+              randomEventId,
+              provider.wallet.publicKey,
+              dummyVaultAccount.publicKey,
+              {
+                accounts: {
+                  eventAccount: dummyEventAccount.publicKey,
+                  authority: provider.wallet.publicKey,
+                  systemProgram: SystemProgram.programId,
+                },
+                signers: [dummyEventAccount],
+              },
+          );
+          return await program.account.eventAccount.fetch(
+              dummyEventAccount.publicKey,
+          );
+        };
         await initializeEvent();
         try {
-            await initializeEvent();
+          await initializeEvent();
         } catch (e) {
-            const error = e as SendTransactionError;
-            // console.log(error);
-            expect(error.toString()).to.include(
-                "This transaction has already been processed"
-            );
+          const error = e as SendTransactionError;
+          expect(error.toString()).to.include(
+              'This transaction has already been processed',
+          );
         }
       });
 });
--- tests/user_integrity.ts
+++ tests/user_integrity.ts
@@ -1,41 +1,44 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import BN from 'bn.js';
-import { assert } from 'chai';
+import {Program} from '@project-serum/anchor';
 
-const { SystemProgram } = anchor.web3;
-import { PublicKey, Keypair } from '@solana/web3.js';
+const {SystemProgram} = anchor.web3;
+import {Keypair} from '@solana/web3.js';
 
 describe('UserIntegrity', () => {
-    const provider = anchor.Provider.env();
-    anchor.setProvider(provider);
-    const program = anchor.workspace.ChooseOption as Program;
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.ChooseOption as Program;
 
-    const event_id: Number = 10;
-    const event_vault_account: Keypair = anchor.web3.Keypair.generate();
-    const event_account: Keypair = anchor.web3.Keypair.generate();
+  const eventId: Number = 10;
+  const eventVaultAccount: Keypair = anchor.web3.Keypair.generate();
+  const eventAccount: Keypair = anchor.web3.Keypair.generate();
 
-    const user_account: Keypair = anchor.web3.Keypair.generate();
+  const userAccount: Keypair = anchor.web3.Keypair.generate();
 
-    before(async () => {
-        await program.rpc.initializeEvent(event_id, provider.wallet.publicKey, event_vault_account.publicKey, {
-            accounts: {
-              eventAccount: event_account.publicKey,
-              authority: provider.wallet.publicKey,
-              systemProgram: SystemProgram.programId,
-            },
-            signers: [event_account],
-          });
+  before(async () => {
+    await program.rpc.initializeEvent(
+        eventId,
+        provider.wallet.publicKey,
+        eventVaultAccount.publicKey,
+        {
+          accounts: {
+            eventAccount: eventAccount.publicKey,
+            authority: provider.wallet.publicKey,
+            systemProgram: SystemProgram.programId,
+          },
+          signers: [eventAccount],
+        },
+    );
+  });
+
+  it('Initialize a UserAccount for given event', async () => {
+    await program.rpc.initializeUser({
+      accounts: {
+        userAccount: userAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [userAccount],
     });
-
-    it('Initialize a UserAccount for given event', async () => {
-        await program.rpc.initializeUser({
-            accounts: {
-                userAccount: user_account.publicKey,
-                user: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId,
-            },
-            signers: [user_account],
-        })
-	});
+  });
 });
