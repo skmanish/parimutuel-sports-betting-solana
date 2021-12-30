cae052aacbe117ee347221ad06048055c78e3a28
Manish <manish.mks.999@gmail.com>
2021-12-31 04:13:15 +0530

    Add a test to fetch all created events

--- tests/event_integrity.ts
+++ tests/event_integrity.ts
@@ -5,32 +5,38 @@ import chaiAsPromised from 'chai-as-promised';
 chai.use(chaiAsPromised);
 
 const {SystemProgram} = anchor.web3;
-import {Keypair, SendTransactionError} from '@solana/web3.js';
+import {Keypair, PublicKey} from '@solana/web3.js';
 
 describe('EventIntegrity', () => {
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.ChooseOption as Program;
 
+  const createEventAccount = async (
+      eventId: Number, eventAccount: Keypair, vaultAccount: PublicKey) => {
+    await program.rpc.initializeEvent(
+        eventId,
+        provider.wallet.publicKey,
+        vaultAccount,
+        {
+          accounts: {
+            eventAccount: eventAccount.publicKey,
+            authority: provider.wallet.publicKey,
+            systemProgram: SystemProgram.programId,
+          },
+          signers: [eventAccount],
+        },
+    );
+  };
   it('Initialize an EventAccount with random event_id and vault', async () => {
     const dummyEventAccount: Keypair = anchor.web3.Keypair.generate();
     const dummyVaultAccount: Keypair = anchor.web3.Keypair.generate();
     // Returns a random integer from 0 to 99:
     const randomEventId = Math.floor(Math.random() * 100);
-
-    await program.rpc.initializeEvent(
+    await createEventAccount(
         randomEventId,
-        provider.wallet.publicKey,
-        dummyVaultAccount.publicKey,
-        {
-          accounts: {
-            eventAccount: dummyEventAccount.publicKey,
-            authority: provider.wallet.publicKey,
-            systemProgram: SystemProgram.programId,
-          },
-          signers: [dummyEventAccount],
-        },
-    );
+        dummyEventAccount,
+        dummyVaultAccount.publicKey);
     const eventAccount: any = await program.account.eventAccount.fetch(
         dummyEventAccount.publicKey,
     );
@@ -42,5 +48,17 @@ describe('EventIntegrity', () => {
     assert.ok(eventAccount.option5BalanceCents === 0);
     assert.deepStrictEqual(eventAccount.authority, provider.wallet.publicKey);
     assert.deepStrictEqual(eventAccount.vault, dummyVaultAccount.publicKey);
+  });
+
+  it('Initialize multiple EventAccount and fetch', async () => {
+    const dummyVaultAccount: Keypair = anchor.web3.Keypair.generate();
+
+    const eventAccount1 = anchor.web3.Keypair.generate();
+    await createEventAccount(1, eventAccount1, dummyVaultAccount.publicKey);
+    const eventAccount2 = anchor.web3.Keypair.generate();
+    await createEventAccount(2, eventAccount2, dummyVaultAccount.publicKey);
+
+    const eventAccounts = await program.account.eventAccount.all();
+    assert.ok(eventAccounts.length >= 2);
   });
 });
