9aaa7511fcd39c801e71ad8268e80a7c5415b7ad
Manish <manish.mks.999@gmail.com>
2021-12-31 13:03:25 +0530

    Proper linkage of backend and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
+/* eslint-disable require-jsdoc */
 import './App.css';
-import { useState } from 'react';
-import { Connection, PublicKey } from '@solana/web3.js';
-import { Program, Provider, web3 } from '@project-serum/anchor';
+import {useState} from 'react';
+import {Connection, PublicKey} from '@solana/web3.js';
+import {Program, Provider, web3} from '@project-serum/anchor';
 import idl from './idl.json';
 
-import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
-import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import {getPhantomWallet} from '@solana/wallet-adapter-wallets';
+import {
+  useWallet,
+  WalletProvider,
+  ConnectionProvider,
+} from '@solana/wallet-adapter-react';
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from '@solana/wallet-adapter-react-ui';
 
-const wallets = [ getPhantomWallet() ]
+const wallets = [getPhantomWallet()];
 
-const { SystemProgram, Keypair } = web3;
+const {SystemProgram, Keypair} = web3;
 const baseAccount = Keypair.generate();
 const opts = {
-  preflightCommitment: "processed"
-}
+  preflightCommitment: 'processed',
+};
 const programID = new PublicKey('6zHwdgEhvbYQ7H3Rf3kc3pQSsQcctv89QnoNYQpquVHM');
 // const programID = new PublicKey(idl.metadata.address);
 
@@ -22,56 +30,60 @@ function App() {
   const [value, setValue] = useState('');
   const [dataList, setDataList] = useState([]);
   const [input, setInput] = useState('');
-  const wallet = useWallet()
+  const wallet = useWallet();
 
   async function getProvider() {
     /* create the provider and return it to the caller */
     /* network set to local network for now */
-    const network = "http://127.0.0.1:8899";
+    const network = 'http://127.0.0.1:8899';
     const connection = new Connection(network, opts.preflightCommitment);
 
     const provider = new Provider(
-      connection, wallet, opts.preflightCommitment,
+        connection, wallet, opts.preflightCommitment,
     );
     return provider;
   }
 
-  async function initialize() {    
+  async function initialize() {
     const provider = await getProvider();
-    /* create the program interface combining the idl, program ID, and provider */
+    /* create the program interface combining the idl,
+       program ID, and provider
+    */
     const program = new Program(idl, programID, provider);
-    console.log("ProgramID", programID);
+    console.log('ProgramID', programID);
     try {
       /* interact with the program via rpc */
-      await program.rpc.initialize("Hello World", {
+      await program.rpc.initialize('Hello World', {
         accounts: {
           baseAccount: baseAccount.publicKey,
           user: provider.wallet.publicKey,
           systemProgram: SystemProgram.programId,
         },
-        signers: [baseAccount]
+        signers: [baseAccount],
       });
 
-      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      const account = await program.account.baseAccount.fetch(
+          baseAccount.publicKey);
       console.log('account: ', account);
       setValue(account.data.toString());
       setDataList(account.dataList);
     } catch (err) {
-      console.log("Transaction error: ", err);
+      console.log('Transaction error: ', err);
     }
   }
 
   async function update() {
-    if (!input) return
+    if (!input) return;
     const provider = await getProvider();
     const program = new Program(idl, programID, provider);
     await program.rpc.update(input, {
       accounts: {
-        baseAccount: baseAccount.publicKey
-      }
+        baseAccount: baseAccount.publicKey,
+      },
     });
 
-    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey);
     console.log('account: ', account);
     setValue(account.data.toString());
     setDataList(account.dataList);
@@ -80,10 +92,13 @@ function App() {
 
   if (!wallet.connected) {
     return (
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop:'100px' }}>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: '100px'}}>
         <WalletMultiButton />
       </div>
-    )
+    );
   } else {
     return (
       <div className="App">
@@ -98,7 +113,7 @@ function App() {
                 <h2>Current value: {value}</h2>
                 <input
                   placeholder="Add new data"
-                  onChange={e => setInput(e.target.value)}
+                  onChange={(e) => setInput(e.target.value)}
                   value={input}
                 />
                 <button onClick={update}>Add data</button>
@@ -124,6 +139,6 @@ const AppWithProvider = () => (
       </WalletModalProvider>
     </WalletProvider>
   </ConnectionProvider>
-)
+);
 
 export default AppWithProvider;
